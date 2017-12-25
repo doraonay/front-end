234b6606abf7578fc72bcd34306b169c3f88155a
doraonay <doraonay@users.noreply.github.com>
2017-12-25 20:52:50 +0300

    react-hw1

---
+++ react-hw1/src/components/listContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import I from "immutable";
+
+class ListContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const items = this.props.todoItems;
+
+    return (
+      <div className="list-container">
+        {
+          items.map((each, idx) => {
+            return (
+              <div key={idx} className="each-todo">
+                {each}
+                
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default ListContainer;
---
+++ react-hw1/src/components/reducer.js
@@ -0,0 +1,17 @@
+import I from "immutable";
+
+function reducer(state = I.Map(), action) {
+  switch (action.type) {
+    case "ADD_CURRENT_INPUT":
+      return state.set("currentInput", action.data);
+    case "ADD_TODO_ITEM":
+      const currentItems = state.get("todoItems", I.List());
+
+      return state.set("todoItems", currentItems.push(action.data));
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;
